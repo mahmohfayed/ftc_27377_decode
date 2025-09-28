5c61af5577978245506fcb9e34dd925f65092eb8
zainelhady <zainelhady0@gmail.com>
2025-09-28 18:20:37 -0400

    first working Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -12,69 +12,75 @@ import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(16.2)
-            .forwardZeroPowerAcceleration(-25.9346931313679598)
-            .lateralZeroPowerAcceleration(-67.342491844080064)
-            .translationalPIDFCoefficients(new PIDFCoefficients(
-                    0.03,
-                    0,
-                    0,
-                    0.015
-            ))
-            .translationalPIDFSwitch(4)
-            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(
-                    0.4,
-                    0,
-                    0.005,
-                    0.0006
-            ))
-            .headingPIDFCoefficients(new PIDFCoefficients(
-                    0.8,
-                    0,
-                    0,
-                    0.01
-            ))
-            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(
-                    2.5,
-                    0,
-                    0.1,
-                    0.0005
-            ))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(
-                    0.1,
-                    0,
-                    0.00035,
-                    0.6,
-                    0.015
-            ))
-            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(
-                    0.02,
-                    0,
-                    0.000005,
-                    0.6,
-                    0.01
-            ))
-            .drivePIDFSwitch(15)
-            .centripetalScaling(0.0005);
+    public static FollowerConstants followerConstants = new FollowerConstants();
+//            .mass(9)
+//            .forwardZeroPowerAcceleration(-25.9346931313679598)
+//            .lateralZeroPowerAcceleration(-67.342491844080064)
+//            .translationalPIDFCoefficients(new PIDFCoefficients(
+//                    0.03,
+//                    0,
+//                    0,
+//                    0.015
+//            ))
+//            .translationalPIDFSwitch(4)
+//            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(
+//                    0.4,
+//                    0,
+//                    0.005,
+//                    0.0006
+//            ))
+//            .headingPIDFCoefficients(new PIDFCoefficients(
+//                    0.8,
+//                    0,
+//                    0,
+//                    0.01
+//            ))
+//            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(
+//                    2.5,
+//                    0,
+//                    0.1,
+//                    0.0005
+//            ))
+//            .drivePIDFCoefficients(new FilteredPIDFCoefficients(
+//                    0.1,
+//                    0,
+//                    0.00035,
+//                    0.6,
+//                    0.015
+//            ))
+//            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(
+//                    0.02,
+//                    0,
+//                    0.000005,
+//                    0.6,
+//                    0.01
+//            ))
+//            .drivePIDFSwitch(15)
+//            .centripetalScaling(0.0005);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
             .leftFrontMotorName("frontLeftMotor")
             .leftRearMotorName("backLeftMotor")
             .rightFrontMotorName("frontRightMotor")
             .rightRearMotorName("backRightMotor")
-            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .xVelocity(78.261926752421046666666666666667)
-            .yVelocity(61.494551922189565);
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+//            .xVelocity(78.261926752421046666666666666667)
+//            .yVelocity(61.494551922189565);
 
     public static PinpointConstants localizerConstants = new PinpointConstants()
-            .forwardPodY(0.75)
-            .strafePodX(-6.6)
-            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .forwardPodY(-8)
+            .strafePodX(-6.5)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
             .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
     /**
@@ -85,23 +91,23 @@ public class Constants {
      The BEZIER_CURVE_SEARCH_LIMIT should typically be left at 10 and shouldn't be changed.
      */
 
-    public static PathConstraints pathConstraints = new PathConstraints(
-            0.995,
-            0.1,
-            0.1,
-            0.009,
-            50,
-            1.25,
-            10,
-            1
-    );
+//    public static PathConstraints pathConstraints = new PathConstraints(
+//            0.995,
+//            0.1,
+//            0.1,
+//            0.009,
+//            50,
+//            1.25,
+//            10,
+//            1
+//    );
 
     //Add custom localizers or drivetrains here
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .mecanumDrivetrain(driveConstants)
                 .pinpointLocalizer(localizerConstants)
-                .pathConstraints(pathConstraints)
+//                .pathConstraints(pathConstraints)
                 .build();
     }
 }
