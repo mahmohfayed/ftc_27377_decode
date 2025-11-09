70d6cdb88e6282616d93639af125b66345359776
NoT-MaRYaM <othman_maryam@icloud.com>
2025-11-09 15:52:56 -0500

    Camera, intake, and apriltag pattern

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+// The intake is a single motor
+public class Intake {
+    private DcMotor intakeMotor;
+
+
+    public void init(HardwareMap hardwareMap) {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+
+    }
+    public void intake(double power) {
+        intakeMotor.setPower(power);
+    }
+    public void stop() {
+        intakeMotor.setPower(0);
+    }
+}
+// left bumper to .25
+// right bumper to defualt(0.8)
+//dpad_up increase speed .25
+// dpad_down decrease by .25
+//+ 8
