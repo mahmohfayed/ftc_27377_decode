3d02ec402a133148c1309f267a64ab772caa7c01
NoT-MaRYaM <othman_maryam@icloud.com>
2025-11-14 20:38:52 -0500

    Loader and Shooter subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Loader.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+//single motor that loads balls
+public class Loader {
+
+    private DcMotor loaderMotor;
+
+    public void init (HardwareMap hardwareMap){
+        loaderMotor = hardwareMap.get(DcMotor.class,"loaderMotor");
+        loaderMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void setLoaderMotor (double power) {
+        loaderMotor.setPower(power);
+    }
+    public void stop(){loaderMotor.setPower(0);}
+}
