ca86384511caa3634910f439208d09e3e2c48376
Jasrah212 <Meeme3534@gmail.com>
2025-11-16 14:40:55 -0500

    ggwa3egf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/shooter.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 //(name = "Shooter PID")
-public class shooter {
+public class Shooter {
 
     DcMotorEx shooter;
 
